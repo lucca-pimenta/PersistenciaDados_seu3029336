aa43934c54f590295fde890e0a94fd1dc4196e6d
Lucca Pimenta <lucca.pimenta@aluno.ifsp.edu.br>
2025-11-27 20:38:22 -0300

    Add files via upload

---
+++ src/main/java/com/LuccaPOO/api_produtos/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.LuccaPOO.api_produtos.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.LuccaPOO.api_produtos.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    List<Produto> findByNome(String nome);
+    List<Produto> findByNomeContaining(String parteDoNome);
+} 
